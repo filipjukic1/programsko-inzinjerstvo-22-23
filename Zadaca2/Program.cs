e67d50fa7e68ddc558f01021dfe80b4ceeae094e
filipjukic1 <82264519+filipjukic1@users.noreply.github.com>
2023-01-29 11:31:55 +0100

    Add files via upload

--- Zadaca2/Program.cs
+++ Zadaca2/Program.cs
@@ -1,15 +1,46 @@
-﻿using System;
+﻿using Solid.Model;
+using Solid.QA;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zadaca2
+namespace Solid
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Terminal terminalSmallTrain = new Terminal(Vlak.Size.Small);
+            Terminal terminalLargeTrain = new Terminal(Vlak.Size.Large);
+
+            Zaposlenik employeeSmallTrain = new Zaposlenik("Marac", 0.1);
+            Zaposlenik employeeLargeTrain = new Zaposlenik("MagnusCArlsen", 0.21);
+
+
+            Vozilo car = new Vozilo(Vozilo.Type.Car, 50, 5);
+            Vozilo van = new Vozilo(Vozilo.Type.Car, 8, 20);
+            Vozilo bus = new Vozilo(Vozilo.Type.Bus, 20, 30);
+            Vozilo truck = new Vozilo(Vozilo.Type.Truck, 5, 60);
+
+            terminalSmallTrain.ParkVehicle(car, employeeSmallTrain);
+            terminalSmallTrain.ParkVehicle(van, employeeSmallTrain);
+            terminalLargeTrain.ParkVehicle(bus, employeeLargeTrain);
+            terminalLargeTrain.ParkVehicle(truck, employeeLargeTrain);
+
+            terminalSmallTrain.GetTicketEarnings();
+            terminalLargeTrain.GetTicketEarnings();
+
+            Console.WriteLine();
+
+            employeeSmallTrain.GetEarnings();
+            employeeLargeTrain.GetEarnings();
+
+            Console.WriteLine();
+
+            Test test = new Test();
+            test.TestTrainSize();
         }
     }
 }
---
+++ Zadaca2/Zaposlenik.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.Model
+{
+    public class Zaposlenik
+    {
+        private string name;
+        private double salary;
+        private double earnings;
+
+        public Zaposlenik(string name, double salary)
+        {
+            this.name = name;
+            this.earnings = 0.0;
+            this.salary = salary;
+        }
+
+        public void CheckVehicle(Vozilo Vozilo, Vlak Vlak)
+        {
+            if (Vlak.GetCapacity() > 0)
+            {
+                Vlak.SetCapacity();
+                earnings += Vozilo.GetFare() * salary;
+
+                if (Vozilo.GetGasSize() < Const.SIZE_LOW)
+                {
+                    Console.WriteLine(name + " " + Vozilo.GetSizeType().ToString() + " velicina je " + Vozilo.GetGasSize() + "% puna");
+                    Console.WriteLine("Tankam");
+                    Vozilo.SetGasSize();
+                    Console.WriteLine(name + "' " + Vozilo.GetSizeType().ToString() + " velicina je " + Vozilo.GetGasSize() + "% puna");
+                }
+
+                if (Vozilo.GetBatterySize() < Const.SIZE_LOW)
+                {
+                    Console.WriteLine(name + " " + Vozilo.GetSizeType().ToString() + " baterija je  " + Vozilo.GetBatterySize() + "% puna");
+                    Console.WriteLine("Punim");
+                    Vozilo.SetBatterySize();
+                    Console.WriteLine(name + " " + Vozilo.GetSizeType().ToString() + " baterija je " + Vozilo.GetBatterySize() + "% puna");
+
+                }
+            }
+        }
+
+
+        public string GetName() => name;
+        public void GetEarnings() =>
+            Console.WriteLine(name + "Zarada je" + earnings);
+    }
+}
